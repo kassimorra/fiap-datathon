f11dc2bf604aba73002168b9155a3de069005068
von-fss <felipe.ruiz.s@gmail.com>
2025-02-25 18:16:09 -0300

    dockering data pipeline

--- data_pipeline/pipeline.py
+++ data_pipeline/pipeline.py
@@ -1,10 +1,9 @@
-from _utils import load_all
-from _process import ProcessTrain, ProcessItem
-from _database import DatabaseFull
+from utils._utils import load_all
+from utils._process import ProcessTrain, ProcessItem
+from utils._database import DatabaseFull
 
 # Load Train and Item folders
-# load_all(ProcessTrain, ProcessItem)
-# load_all(ProcessItem)
+load_all(ProcessTrain, ProcessItem)
 
 # Create the join table from train and item
 database_full: DatabaseFull = DatabaseFull()
